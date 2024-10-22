a99a02f7c8bb13c5e8171cd94e407975276993e0
AntonZhigunov <azhigunov79@gmail.com>
2024-10-22 18:04:36 +0300

    Add files via upload

---
+++ Module_1_4.py
@@ -0,0 +1,8 @@
+Homework_of_tests = 'всего задач:'
+Number_of_operations = 12
+Time_for_tests = 'затрачено часов:'
+Time_of_operation = 1.5
+Course_name = 'Курс: Python,'
+Time_one_test = 'Время на одно задание:'
+print(Course_name, Homework_of_tests, Number_of_operations,',', Time_for_tests, Time_of_operation,',', Time_one_test, Time_of_operation / Number_of_operations,'.')
+
